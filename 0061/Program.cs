e15ab19bd5d46da409a4f1ed1db82083b1f6a962
Alexei <datlife2015@yandex.ru>
2022-11-08 10:05:36 +0300

    Написана программа копирования массива

--- 0061/Program.cs
+++ 0061/Program.cs
@@ -9,23 +9,55 @@ double FinfPointY(double b2, double k2, double x)
     return k2 * x + b2;
 }
 //Показать числа Фибоначчи
-double Fibonacci(int n)
+double Fibonacci(double number)
 {
- int fibo = 0;
- int f1 = 1;
- int f2 = 1;
- if(n == 1 || n == 2) return 1;
+    double f1 = 1;
+    double f2 = 1;
+    double result =0;
+    if(number == 1 || number == 2) 
+    return 1;
     else   
     {
-        for(int i = 2; i < n; i++)
+        for(double i = 2; i < number; i++)
         {
-            fibo = f1 + f2;
+            result = f1 + f2;
             f1 = f2; 
-            f2 = fibo;
+            f2 = result;
         }
-        return fibo;
+        return result;
     } 
 }
+// Написать программу копирования массива
+int[]CreateArray(int size, int min, int max)
+{
+    int[]array = new int[size];
+    Random rnd= new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i]=rnd.Next(min, max);
+    }
+    return array;
+}
+void PrintArray(int[]array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i<array.Length-1)Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}   
+int[]CopyArray(int[]array)
+{
+    int[]newarray=new int[array.Length];
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        newarray[i]=array[i];
+    }
+    return newarray;
+}
+
 Console.WriteLine("Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы");
 Console.Write("Введите B1: ");
 double b1 = double.Parse(Console.ReadLine() ?? "0");
@@ -46,7 +78,14 @@ else
 
 Console.WriteLine("Показать числа Фибоначчи");
 Console.Write("Введите значение: ");
-int n = int.Parse(Console.ReadLine() ?? "0");
-Fibonacci(n);40
-for (int i = 1; i <= n; i++)
- Console.Write($"f({i}) = {Fibonacci(i)}");
+double num = Convert.ToInt32 (Console.ReadLine());
+Fibonacci(num);
+for (double i = 1; i <= num; i++)
+ Console.WriteLine($"{i} => {Fibonacci(i)}");
+Console.WriteLine();
+
+Console.WriteLine("Написать программу копирования массива");
+ int[]arr = CreateArray(10, 1, 10);
+ PrintArray(arr);
+ int[]newarr = CopyArray(arr);
+ PrintArray(newarr);
